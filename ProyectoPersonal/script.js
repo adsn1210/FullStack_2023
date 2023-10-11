2951616117c3d12a5767685aae51652eacd660aa
FullStack_20232 <adriansalazarn@gmail.com>
2023-10-11 14:06:55 +0200

    creacion dia 1 de proyecto personal22

--- ProyectoPersonal/script.js
+++ ProyectoPersonal/script.js
@@ -1,5 +1,6 @@
 
 
+// Imagenes Slides
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slide');
 
@@ -21,3 +22,19 @@ function prevSlide() {
 
 setInterval(nextSlide, 3000); // Cambia la imagen automáticamente cada 3 segundos
 
+
+
+
+// Solapar header con Nav
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+        var nav = document.getElementById('nav');
+        var offset = nav.offsetTop-58;
+
+        if (window.scrollY > offset) {
+            nav.classList.add('fixed-nav');
+        } else {
+            nav.classList.remove('fixed-nav');
+        }
+    });
+});
